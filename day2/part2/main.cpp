509c3180066340dc56b02dfda253b36eb7e9574e
DavesUnderscoreTreehouse <davet6243+github@gmail.com>
2025-12-02 16:47:27 +0000

    day2 part2 complete

--- day2/part2/main.cpp
+++ day2/part2/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() 
 {
   // Filepath for input data
-  string filepath = "../testinput.txt";
+  string filepath = "../input.txt";
   // Read from the text file
   ifstream f(filepath);
 
@@ -23,41 +23,49 @@ int main()
 
     for (__int64 id = num1; id <= num2; id++) {
       string idStr = to_string(id);
-      bool part1invalid = false;
-      bool part2invalid = true;
+      bool invalid = false;
       // remove odd length nums
       if (((idStr.length() % 2) == 0) && (idStr.substr(0, (idStr.length() / 2)) == idStr.substr(idStr.length() / 2, (idStr.length() / 2)))) {
-        part1invalid = true;
-        cout << "first if invalid " << idStr << "\n";
+        invalid = true;
+        //cout << "part 1 catch - invalid: " << idStr << "\n";
       }
       else {
-        for (int i = 1; i < idStr.length() / 2; i++) {          // iterate through pattern lengths
-          for (int j = 0; j < idStr.length() / i; j++) {        // iterate through positions
-            if (idStr.substr(0, i) == idStr.substr(i * j, i)) {
-              cout << "Invalid: " << idStr.substr(0,i) << ", " << idStr.substr(i * j, i) << "\n";
-            }
-            else {
-              part2invalid = false;
-              cout << "Valid: " << idStr.substr(0,i) << ", " << idStr.substr(i * j, i) << "\n";
-            }
+        if (idStr.length() == 3) {
+          if ((idStr[0] == idStr[1]) && (idStr[0] == idStr[2])) {
+            invalid = true;
+            //cout << "3 digit match - invalid: " << idStr << "\n";
           }
-          if (part2invalid) {
-            part1invalid = true;
-            cout << "part 2 override catch\n";
-          }
-          else {
-            cout << "pattern length " << i << " checked\n";
-            part2invalid = true;
+        }
+        else {
+          for (int i = 1; i < idStr.length() / 2; i++) {          // iterate through pattern lengths
+            bool part2internalinvalid = true;
+            for (int j = 0; j < idStr.length() / i; j++) {        // iterate through positions
+              if ((idStr.substr(0, i) == idStr.substr(i * j, i)) && ((idStr.length() % i) == 0)) {
+                //cout << "Invalid: " << idStr.substr(0,i) << ", " << idStr.substr(i * j, i) << "\n";
+              }
+              else {
+                part2internalinvalid = false;
+                //cout << "Valid: " << idStr.substr(0,i) << ", " << idStr.substr(i * j, i) << "\n";
+              }
+            }
+            if (part2internalinvalid) {
+              //cout << "internal part 2 flag\n";
+              invalid = true;
+            }
+            //cout << "invalid: " << invalid << "\n";
+            //cout << "pattern length " << i << " checked\n";
           }
         }
       }
-      if (part1invalid || part2invalid) {
-          cout << "Match: " << id << "\n\n";
+      if (invalid) {
+          //cout << "invalid: " << invalid << "\n";
+          //cout << "Match: " << id << "\n\n";
           total += id;
-          //cout << "Total so far: " << total << "\n";6
+          //cout << "Total so far: " << total << "\n";
         }
-      else
-        cout << "\n";
+      // else {
+      //   cout << "No match: " << idStr << "\n\n";
+      // }
     }
   } 
   while (f.peek() != EOF);
