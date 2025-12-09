114faa81178f5f7deb1470627611d7e4eca607c9
DavesUnderscoreTreehouse <davet6243+github@gmail.com>
2025-12-09 01:21:58 +0000

    day8 part1 attempt 'feels close'

--- day8/Main.cpp
+++ day8/Main.cpp
@@ -18,7 +18,7 @@ bool compareJunctions(Junction a, Junction b)
     // If connction distance not the same then
     // returns true for higher total
     if (a.connectionDist != b.connectionDist)
-        return a.connectionDist > b.connectionDist;
+        return a.connectionDist < b.connectionDist;
 
     return (false);
 }
@@ -58,25 +58,25 @@ int main()
   oldDist = -1;
   closeId = -1;
   for (int i = 0; i < lineCount; i++) {
-    //cout << "xDist\t" << "yDist\t" << "zDist\t" << "Dist\n";
+    cout << "xDist\t" << "yDist\t" << "zDist\t" << "Dist\n";
     for (int j = 0; j < lineCount; j++) {
       xDist = junctions[i].x - junctions[j].x;
       yDist = junctions[i].y - junctions[j].y;
       zDist = junctions[i].z - junctions[j].z;
       dist = sqrt(pow(xDist, 2) + pow(yDist, 2) + pow(zDist, 2));
-      //cout << xDist << "\t" << yDist << "\t" << zDist << "\t" << dist << "\n";
+      cout << xDist << "\t" << yDist << "\t" << zDist << "\t" << dist << "\n";
       if ((dist == 0) && (j == 0)) {
         oldDist = 100000;
       }
       if ((dist != 0) && ((dist < oldDist) || (j == 0))) {
         oldDist = dist;
-        closeId = junctions[j].connectedId;
+        closeId = junctions[j].id;
       }
     }
     junctions[i].connectedId = closeId;
     junctions[i].connectionDist = oldDist;
-    //cout << "Junction:\n";
-    //cout << junctions[i].x << "\t" << junctions[i].y << "\t" << junctions[i].z << "\t" << junctions[i].connectionDist << "\n\n";
+    cout << "Junction:\n";
+    cout << junctions[i].x << "\t" << junctions[i].y << "\t" << junctions[i].z << "\t" << junctions[i].connectionDist << "\n\n";
   }
   
   // sort by distance
@@ -84,36 +84,55 @@ int main()
   
   // if junctions in same circuit, ignore
   // else, join
-  for (int i = 0; i < (sizeof(junctions) / sizeof(junctions[0])); i++) {
+  for (int i = 0; i < ((sizeof(junctions) / sizeof(junctions[0])) - 1); i++) {
     // find connected junc obj
-    Junction connectedJunction;
+    if (junctions[i].connectedId == -1) {
+      continue;
+    } 
     int j;
-    for (j = 0; j < (sizeof(junctions) / sizeof(junctions[0])); j++) {
+    for (j = 0; j < ((sizeof(junctions) / sizeof(junctions[0])) - 1); j++) {
       if (junctions[i].connectedId == junctions[j].id) {
-        connectedJunction = junctions[j];
         cout << "Circuit match found: " << junctions[i].id << " connected to " << junctions[j].id << "\n";
+        break;
       }
     }
     if ((junctions[i].circuitNo == -1) && (junctions[j].circuitNo == -1)) {
       // circuit tags starting at 1
       junctions[i].circuitNo = circuitSizes.size();
-      circuitSizes.push_back(2);
+      circuitSizes.push_back(1);
+      cout << "New circuit created: " << junctions[i].circuitNo << " size " << circuitSizes.back() << "\n";
     }
     else if (junctions[i].circuitNo == -1) {
       junctions[i].circuitNo = junctions[j].circuitNo;
       circuitSizes.at(junctions[j].circuitNo)++;
+      cout << "Junction " << junctions[i].id << " added to circuit " << junctions[j].circuitNo << " new size " << circuitSizes.at(junctions[j].circuitNo) << "\n";
     }
     else if (junctions[j].circuitNo == -1) {
       junctions[j].circuitNo = junctions[i].circuitNo;
       circuitSizes.at(junctions[i].circuitNo)++;
+      cout << "Junction " << junctions[j].id << " added to circuit " << junctions[i].circuitNo << " new size " << circuitSizes.at(junctions[i].circuitNo) << "\n";
     }
     else if (junctions[i].circuitNo == junctions[j].circuitNo) {
-      
+      continue; // ignore
     }
-    //  ignore
+    else {
+      // merge circuits
+      int oldCircuitNo = junctions[j].circuitNo;
+      int newCircuitNo = junctions[i].circuitNo;
+      circuitSizes.at(newCircuitNo) += circuitSizes.at(oldCircuitNo);
+      circuitSizes.at(oldCircuitNo) = 0;
+      cout << "Merging circuits " << oldCircuitNo << " into " << newCircuitNo << " new size " << circuitSizes.at(newCircuitNo) << "\n";
+      // update all junctions with old circuit no
+      for (int k = 0; k < ((sizeof(junctions) / sizeof(junctions[0])) - 1); k++) {
+        if (junctions[k].circuitNo == oldCircuitNo) {
+          junctions[k].circuitNo = newCircuitNo;
+        }
+      }
+    }
+    cout << "\n";
   }
   // sort
-  sort(circuitSizes.begin(), circuitSizes.end());
+  sort(circuitSizes.begin(), circuitSizes.end(), greater<>());
   
   cout << "Circuit sizes:\n";
   for (int i : circuitSizes) {
